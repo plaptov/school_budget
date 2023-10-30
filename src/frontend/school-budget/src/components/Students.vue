bc77c2497d88c299816e90742523bbcf25cb1890
Pavel Laptov <pavel.laptov@ya.ru>
2023-10-31 00:09:13 +0300

    basic crud

--- src/frontend/school-budget/src/components/Students.vue
+++ src/frontend/school-budget/src/components/Students.vue
@@ -1,23 +1,66 @@
 <template>
   <Suspense>
-    <v-responsive>
-      <v-table>
-        <thead>
-          <tr>
-            <th class="text-left">ID</th>
-            <th class="text-left">ФИО</th>
-            <th class="text-left">Комментарий</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="student in students" :key="student.id">
-            <td>{{ student.id }}</td>
-            <td>{{ student.name }}</td>
-            <td>{{ student.comment }}</td>
-          </tr>
-        </tbody>
-      </v-table>
-    </v-responsive>
+    <v-container fluid>
+      <v-row>
+        <v-col>
+          <v-btn prepend-icon="$plus" @click="addStudent()"> Добавить </v-btn>
+        </v-col>
+      </v-row>
+
+      <v-row v-if="editingStudent">
+        <v-col>
+          <v-card>
+            <v-card-title>
+              <span class="text-h5">Редактирование</span>
+            </v-card-title>
+            <v-card-text>
+              <v-row>
+                <v-col>
+                  <v-text-field v-model="editingStudent.name" label="ФИО"></v-text-field>
+                </v-col>
+                <v-col>
+                  <v-text-field v-model="editingStudent.comment" label="Комментарий"></v-text-field>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col>
+                  <div style="display:flex; gap: 10px">
+                    <v-btn color="primary" @click="saveStudent()">Сохранить</v-btn>
+                    <v-btn color="error" @click="editingStudent = null">Отменить</v-btn>
+                  </div>
+                </v-col>
+              </v-row>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+
+      <v-row>
+        <v-col>
+          <v-table>
+            <thead>
+              <tr>
+                <th/>
+                <th class="text-left">ID</th>
+                <th class="text-left">ФИО</th>
+                <th class="text-left">Комментарий</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="student in students" :key="student.id">
+                <td>
+                  <v-icon icon="$edit" @click="editingStudent = student"/>
+                  <v-icon icon="$delete" @click="deleteStudent(student)"/>
+                </td>
+                <td>{{ student.id }}</td>
+                <td>{{ student.name }}</td>
+                <td>{{ student.comment }}</td>
+              </tr>
+            </tbody>
+          </v-table>
+        </v-col>
+      </v-row>
+    </v-container>
     <template #fallback> Загрузка... </template>
   </Suspense>
 </template>
@@ -29,8 +72,33 @@ import { CrudService } from "@/services/crud-service";
 
 const students = ref<Student[]>([]);
 const studentsService = new CrudService<Student>("student");
+const editingStudent = ref<Student | null>(null);
 
 onBeforeMount(async () => {
   students.value = await studentsService.getAll();
 });
+
+function addStudent(): void {
+  editingStudent.value = {
+    id: 0,
+    name: "",
+    comment: "",
+  }
+}
+
+async function saveStudent(): Promise<void> {
+  const student = editingStudent.value!;
+  if (student.id) {
+    await studentsService.update(student);
+  } else {
+    const newStudent = await studentsService.create(student);
+    students.value.push(newStudent);
+  }
+  editingStudent.value = null;
+}
+
+async function deleteStudent(student: Student): Promise<void> {
+  await studentsService.delete(student.id);
+  students.value = students.value.filter((s) => s.id !== student.id);
+}
 </script>
--- src/frontend/school-budget/src/services/base-api-service.ts
+++ src/frontend/school-budget/src/services/base-api-service.ts
@@ -1,26 +1,39 @@
+const jsonHeaders = new Headers({ "Content-Type": "application/json" });
+type paramsType = string[][] | Record<string, string> | string | URLSearchParams;
+
 export class BaseApiService {
-  protected getRequest<T>(url: string): Promise<T> {
+  protected getRequest<T>(url: string, queryParams?: paramsType): Promise<T> {
+    if (queryParams) {
+      url += `?${new URLSearchParams(queryParams)}`;
+    }
+
     return this.fetch(url);
   }
 
   protected postRequest<T>(url: string, body: any): Promise<T> {
-    return this.fetch(url, { method: 'POST', body });
+    return this.fetch(url, { method: 'POST', headers: jsonHeaders, body: JSON.stringify(body) });
   }
 
   protected putRequest<T>(url: string, body: any): Promise<T> {
-    return this.fetch(url, { method: 'PUT', body });
+    return this.fetch(url, { method: 'PUT', headers: jsonHeaders, body: JSON.stringify(body) });
   }
 
-  protected deleteRequest<T>(url: string): Promise<T> {
-    return this.fetch(url, { method: 'DELETE' });
+  protected deleteRequest(url: string): Promise<void> {
+    return this.fetchVoid(url, { method: 'DELETE' });
   }
 
-  protected fetch<T>(url: string, init?: RequestInit): Promise<T> {
-    return fetch(url, init).then((response) => {
-      if (!response.ok) {
-        throw new Error(response.statusText);
-      }
-      return response.json() as Promise<T>;
-    });
+  protected async fetch<T>(url: string, init?: RequestInit): Promise<T> {
+    const response = await fetch(url, init);
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    return await response.json() as T;
+  }
+
+  protected async fetchVoid(url: string, init?: RequestInit): Promise<void> {
+    const response = await fetch(url, init);
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
   }
 }
